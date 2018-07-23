9081b90ea5a22a82fbe6dde4a38ddd90dcd34bda
James Kageni <james.kageni@gmail.com>
2018-07-23 12:41:26 +0300

    added the random quote API and made prelim styling changes

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function randomQuote() {
+  $.ajax({
+      url: "https://api.forismatic.com/api/1.0/?",
+      dataType: "jsonp",
+      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+      success: function( response ) {
+        $("#random-quote").text(response.quoteText + response.quoteAuthor);
+      }
+  });
+}
+
+randomQuote();
