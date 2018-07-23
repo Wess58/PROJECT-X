5822ffaa7aee65ba0e057ba06f4702d5b59c5a3e
James Kageni <james.kageni@gmail.com>
2018-07-23 14:18:34 +0300

    final styling experiments for quote

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
+//This is the quto randomizer using the forismatic API and ajax.
+
 function randomQuote() {
   $.ajax({
       url: "https://api.forismatic.com/api/1.0/?",
       dataType: "jsonp",
       data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
       success: function( response ) {
-        $("#random-quote").text(response.quoteText + response.quoteAuthor);
+        $("#quotes").html(response.quoteText + "&dash;" + response.quoteAuthor + "&dash;");
       }
   });
 }
-
 randomQuote();
