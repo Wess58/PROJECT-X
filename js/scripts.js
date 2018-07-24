946033df3d4cfddbaf9430583eb7b8bc28dfe08a
Kennedy Nyaga <gnyagaken@gmail.com>
2018-07-24 15:36:21 +0300

    background changes, login set-up, navbar

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,27 @@ function randomQuote() {
   });
 }
 randomQuote();
+
+
+// click event log-in
+$(document).ready(function(){
+  $('#login-nav').click(function(){
+    $('#id01').css('display','block');
+  });
+
+  $('#login-cancel').click(function(){
+    $('#id01').css('display','none');
+  });
+
+  $('.close').click(function(){
+    $('#id01').css('display','none');
+  });
+  // Get the modal
+var modal = document.getElementById('id01');
+
+$(document).click(function(event){
+  if(event.target==modal){
+    $('#id01').css('display','none');
+  }
+});
+ });
