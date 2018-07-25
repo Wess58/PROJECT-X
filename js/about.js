b254b3649f5bd3b4025ba56776e078d968bc021c
youngkhalid <youngkhalifah98@gmail.com>
2018-07-25 08:08:00 +0300

    contactus file created

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
 
 function myFunction() {
-    var x = document.getElementById("show");
+    var x = document.getElementById("myDIV");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
